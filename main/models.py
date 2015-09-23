7ee75bd8933edcda2f6288699ba3eea1bf3329f8
gqdw <aca_jingru@qq.com>
2015-09-23 13:29:07 +0800

    endtime可以为空,表示还没结束

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
 	name = models.CharField(max_length=30)
 	begintime = models.DateField()
-	endtime = models.DateField()
+	endtime = models.DateField(blank=True)
 	todo = models.TextField()
 	def __unicode__(self):
 		return self.name
